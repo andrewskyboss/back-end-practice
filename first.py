cad77f97473562c37b06d328d96b3a459bb83e72
andrewskyboss <andrew.cybossky@gmail.com>
2021-07-04 19:35:25 +0000

    Flask library

--- first.py
+++ first.py
@@ -1,10 +1,12 @@
 def print_message(message):
     print(message)
 
-print_message("Hello, World ")
+
+print_message("Hello, World")
 
 my_list = [1, 2, 3, 4, 5]
 
 for item in my_list:
     print(item)
+
 
